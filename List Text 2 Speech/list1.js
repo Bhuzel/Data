35f2be6b3b474d41fcb76056cb947b96355b3ca0
Bhuzel <159991153+Bhuzel@users.noreply.github.com>
2025-04-05 18:13:35 +0700

    Create list1.js

---
+++ List Text 2 Speech/list1.js
@@ -0,0 +1,22 @@
+const voices = {
+  male: [
+    { name: "Bradley Marshall", id: "waveltts_3786e470-7129-4f01-a263-0801b302acf1" },
+    { name: "Rowan Flynn", id: "waveltts_7a16488d-eba0-4fa3-876a-97fbd57551ca" },
+    { name: "Atlas", id: "waveltts_f5066419-beae-43c6-bf67-d8ad0cec52a5" },
+    { name: "Cassius Grimwood", id: "waveltts_457e2c2d-3446-4c04-ada1-c24f2541fbb1" },
+    { name: "Kaelin Vex", id: "waveltts_ff934526-807a-4210-bfa0-0878516f08c2" },
+    { name: "Julian Stiles", id: "waveltts_50947fa3-1f62-418e-a725-47f96ff5c146" },
+    { name: "Zayn", id: "waveltts_58f3fe09-64b3-42ca-a7a9-b80527accb56" },
+    { name: "Ryder Winston", id: "waveltts_1732bec6-c5a4-43fd-954e-62e1f50d7f38" },
+    { name: "Lucas Brooks", id: "waveltts_28d785c9-1e53-485d-a981-b0c55ba32918" }
+  ],
+  female: [
+    { name: "Calista", id: "waveltts_aaf98444-e4e9-4bd6-9921-b307bbd2689e" },
+    { name: "Serene Loh", id: "waveltts_297d3749-2394-4396-8324-e6fdb26846f0" },
+    { name: "Nandi Khoza", id: "waveltts_9c637c4d-76fb-4568-815a-31947c7a61c1" },
+    { name: "Sofía Mariposa", id: "waveltts_e51e20fb-4e89-41a0-9fbe-0f22f73c9557" },
+    { name: "Dakshita", id: "waveltts_309de86f-2324-411f-ab8a-f8689455ec26" }
+  ]
+};
+
+const languages = ["ml-IN", "en-US", "es-ES", "ja-JP", "id-ID", "ko-KR", "ru-RU"];
