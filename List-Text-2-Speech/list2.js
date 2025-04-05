0131fe99121a21e30c77b9644308f7c3d0650795
Bhuzel <159991153+Bhuzel@users.noreply.github.com>
2025-04-05 19:11:40 +0700

    Create list2.js

---
+++ List-Text-2-Speech/list2.js
@@ -0,0 +1,32 @@
+// ambil value nya unruk parameter language, misal "en"
+const languages = [
+    { value: "en", label: "English" },
+    { value: "zh", label: "Chinese (中文)" },
+    { value: "ja", label: "Japanese (日本語)" },
+    { value: "es", label: "Spanish (Español)" },
+    { value: "fr", label: "French (Français)" },
+    { value: "de", label: "German (Deutsch)" },
+    { value: "it", label: "Italian (Italiano)" },
+    { value: "pt", label: "Portuguese (Português)" },
+    { value: "pl", label: "Polish (Polski)" },
+    { value: "ru", label: "Russian (Русский)" },
+    { value: "nl", label: "Dutch (Nederlands)" },
+    { value: "ar", label: "Arabic (العربية)" },
+    { value: "ko", label: "Korean (한국어)" }
+];
+
+
+// ambil name nya untuk parameter name, misal "American Female 1"
+const voices = [
+    { voiceId: "af", voiceIndex: 0, voiceType: "standard", name: "American Female 1" },
+    { voiceId: "af_bella", voiceIndex: 1, voiceType: "standard", name: "American Female 2" },
+    { voiceId: "af_sarah", voiceIndex: 2, voiceType: "standard", name: "American Female 3" },
+    { voiceId: "af_nicole", voiceIndex: 3, voiceType: "standard", name: "American Female 4" },
+    { voiceId: "af_sky", voiceIndex: 4, voiceType: "standard", name: "American Female 5" },
+    { voiceId: "am_adam", voiceIndex: 5, voiceType: "standard", name: "American Male 1" },
+    { voiceId: "am_michael", voiceIndex: 6, voiceType: "standard", name: "American Male 2" },
+    { voiceId: "bf_emma", voiceIndex: 7, voiceType: "standard", name: "British Female 1" },
+    { voiceId: "bf_isabella", voiceIndex: 8, voiceType: "standard", name: "British Female 2" },
+    { voiceId: "bm_george", voiceIndex: 9, voiceType: "standard", name: "British Male 1" },
+    { voiceId: "bm_lewis", voiceIndex: 10, voiceType: "standard", name: "British Male 2" }
+];
