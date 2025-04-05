b847be9cb769208250b890832af0f9cf1bdf3f81
Bhuzel <159991153+Bhuzel@users.noreply.github.com>
2025-04-05 18:27:33 +0700

    Update list1.js

--- List-Text-2-Speech/list1.js
+++ List-Text-2-Speech/list1.js
@@ -1,22 +1,26 @@
-const voices = {
-  male: [
-    { name: "Bradley Marshall", id: "waveltts_3786e470-7129-4f01-a263-0801b302acf1" },
-    { name: "Rowan Flynn", id: "waveltts_7a16488d-eba0-4fa3-876a-97fbd57551ca" },
-    { name: "Atlas", id: "waveltts_f5066419-beae-43c6-bf67-d8ad0cec52a5" },
-    { name: "Cassius Grimwood", id: "waveltts_457e2c2d-3446-4c04-ada1-c24f2541fbb1" },
-    { name: "Kaelin Vex", id: "waveltts_ff934526-807a-4210-bfa0-0878516f08c2" },
-    { name: "Julian Stiles", id: "waveltts_50947fa3-1f62-418e-a725-47f96ff5c146" },
-    { name: "Zayn", id: "waveltts_58f3fe09-64b3-42ca-a7a9-b80527accb56" },
-    { name: "Ryder Winston", id: "waveltts_1732bec6-c5a4-43fd-954e-62e1f50d7f38" },
-    { name: "Lucas Brooks", id: "waveltts_28d785c9-1e53-485d-a981-b0c55ba32918" }
-  ],
-  female: [
-    { name: "Calista", id: "waveltts_aaf98444-e4e9-4bd6-9921-b307bbd2689e" },
-    { name: "Serene Loh", id: "waveltts_297d3749-2394-4396-8324-e6fdb26846f0" },
-    { name: "Nandi Khoza", id: "waveltts_9c637c4d-76fb-4568-815a-31947c7a61c1" },
-    { name: "Sofía Mariposa", id: "waveltts_e51e20fb-4e89-41a0-9fbe-0f22f73c9557" },
-    { name: "Dakshita", id: "waveltts_309de86f-2324-411f-ab8a-f8689455ec26" }
-  ]
-};
+const voices = [
+  { id: "1", name: "Bradley Marshall", gender: "male" },
+  { id: "2", name: "Rowan Flynn", gender: "male" },
+  { id: "3", name: "Atlas", gender: "male" },
+  { id: "4", name: "Cassius Grimwood", gender: "male" },
+  { id: "5", name: "Kaelin Vex", gender: "male" },
+  { id: "6", name: "Julian Stiles", gender: "male" },
+  { id: "7", name: "Zayn", gender: "male" },
+  { id: "8", name: "Ryder Winston", gender: "male" },
+  { id: "9", name: "Lucas Brooks", gender: "male" },
+  { id: "10", name: "Calista", gender: "female" },
+  { id: "11", name: "Serene Loh", gender: "female" },
+  { id: "12", name: "Nandi Khoza", gender: "female" },
+  { id: "13", name: "Sofía Mariposa", gender: "female" },
+  { id: "14", name: "Dakshita", gender: "female" }
+];
 
-const languages = ["ml-IN", "en-US", "es-ES", "ja-JP", "id-ID", "ko-KR", "ru-RU"];
+const languages = {
+  id: "indonesia",
+  en: "english",
+  es: "spanish",
+  ja: "japanese",
+  ko: "korean",
+  ru: "russian",
+  ml: "malayalam"
+};
